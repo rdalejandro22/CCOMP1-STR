a35706426ecef42f7dfec25a6a60d23a14f4b099
rdalejandro22 <alejandro.risco@ucsp.edu.pe>
2016-11-15 00:52:56 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,102 @@
+#include <iostream>
+#include <windows.h>
 #include "miniwin.h"
+
 using namespace miniwin;
-const int TAM = 25;
-struct Coordenadas
-{
-    int x, y;
-};
-struct Pieza
-{
-    Coordenadas pivote;
-    Coordenadas extremos[3];
-};
-void cuadrado(int x, int y){
-    rectangulo_lleno(1 + x * TAM,
-                     1 + y * TAM,
-                     x * TAM + TAM,
-                     y * TAM + TAM);
+int main(){
+    vredimensiona(250,500);
 }
-void crear_Pieza(const Pieza &P){
-    color(VERDE);
-    cuadrado(P.pivote.x, P.pivote.y); //central
-    for(int i = 0; i < 3; i++){
-        cuadrado(P.pivote.x + P.extremos[i].x,
-                 P.pivote.y + P.extremos[i].y);
-    }
+/*char mapa[10][20] = {
+"###################",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"###################"
+};
+char mapa2[10][20] = {
+"###################",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"#                 #",
+"###################"
+};
+char pieza = '@';
+bool ejecucion = true;
+int x = 9;
+int y = 1;
+void reiniciar(){
+    x = 9;
+    y = 1;
 }
+int filallena = 0;
 int main()
 {
-    vredimensiona(500,500);
-    Pieza s1 = {{2,2},{{-1,-1},{0,-1},{1,0}}};
-    crear_Pieza(s1);
-    refresca();
-}
+
+    while(ejecucion==true)
+    {
+        if (filallena==16){
+            int i = 8;
+            for (i;i>1;i--){
+                mapa[i][x]=mapa[i-1][x];}
+        }
+        system("cls");
+        for(int visualizar=0; visualizar<10;visualizar++){
+            cout << mapa[visualizar] << endl;}
+        mapa[y][x]=pieza;
+        system("pause>nul");
+        if(GetAsyncKeyState(VK_DOWN)){
+            int y2 = y+1;
+            if(mapa[y2][x] != '#'){
+                if(mapa[y2][x]=='@'){
+                    mapa[y][x]=='@';
+                    reiniciar();
+                }
+                if(y2==8){
+                    mapa[y][x] = ' ';
+                    y++;
+                    mapa[y][x] = '@';
+                    reiniciar();
+                    filallena++;
+                }
+                else
+                    mapa[y][x] = ' ';
+                    y++;
+                    mapa[y][x] = pieza;
+            }
+        }
+        if(GetAsyncKeyState(VK_RIGHT)){
+            int x2 = x+1;
+            if(mapa[y][x2] != '#'){
+                mapa[y][x] = ' ';
+                x++;
+                mapa[y][x] = pieza;
+            }
+        }
+        if(GetAsyncKeyState(VK_LEFT)){
+            int x2 = x-1;
+            if(mapa[y][x2] != '#'){
+                mapa[y][x] = ' ';
+                x--;
+                mapa[y][x] = pieza;
+            }
+        }
+        if(GetAsyncKeyState(VK_ESCAPE)){
+            ejecucion=false;
+        }
+    }
+
+    system("cls");
+    cout << "GAME OVER";
+    return 0;
+}*/
 
